691623f1ad83f859e41c7a587c4b4057eab9318a
Vesa Rautiainen <veskurau@gmail.com>
2023-11-18 15:55:21 +0200

    Add SQL schema

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    user_id INTEGER REFERENCES users,
+    sent_at TIMESTAMP
+);
